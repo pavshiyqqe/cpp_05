7d94611de803d709777889d4d1df482969d9f00f
Eugene_pavshiy <keki4noob@gmail.com>
2025-11-27 14:29:08 +0100

    123

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,25 @@
 /*   By: yyaniv <yyaniv@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 12:46:49 by yyaniv            #+#    #+#             */
-/*   Updated: 2025/11/27 12:46:51 by yyaniv           ###   ########.fr       */
+/*   Updated: 2025/11/27 14:28:52 by yyaniv           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <exception>
+#include <iostream>
+#include <string>
+
+class Bureaucrat 
+{
+    private:
+        static const int    maxGrade = 150;
+        static const int    minGrade = 1;
+        const std::string   name;
+        int                 grade;
+        Bureaucrat();
+    
+    public:
+        
+};
